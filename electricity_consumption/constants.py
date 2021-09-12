05659a86db6124f9fdc50c441e03dcea76e932cd
vlemeur <vincenlemeur@gmail.com>
2021-09-12 11:32:51 +0200

    update constants and paths

--- electricity_consumption/constants.py
+++ electricity_consumption/constants.py
@@ -1,5 +1,5 @@
 """This module provides general variables settings used in other modules
 """
 
-INFLUXDB_TOKE = ""
+INFLUXDB_TOKE = "XVfGCYx7S_Y_93T9MXSBLr6mUR5VgNicCRkT1vDwwW0tg1E_iR3_6M2tIU2L2-fsSULpmNlYKBNkoD4wUM1aPQ=="
 INFLUXDB_URL = "http://localhost:8086"
