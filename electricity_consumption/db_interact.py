60f3a51ba11be65e3c2deafeb2b2e518b3ade881
vlemeur <vincenlemeur@gmail.com>
2021-09-12 11:46:34 +0200

    wip authentification

--- electricity_consumption/db_interact.py
+++ electricity_consumption/db_interact.py
@@ -1,23 +1,37 @@
 """This module provides function to interact with Influx DB database
 """
 
-from influxdb_client import InfluxDBClient
+from pathlib import Path
 
-from electricity_consumption.constants import INFLUXDB_TOKE, INFLUXDB_URL
+import pandas as pd
+
 from electricity_consumption.paths import PATH_INFLUXDB_MK_FILE
 from electricity_consumption.utils import get_variables_from_mk_file
 
 DICT_INFLUXDB = get_variables_from_mk_file(path_mk_file=PATH_INFLUXDB_MK_FILE)
 
 
-def initiate_influxdb_client() -> InfluxDBClient:
-    """Initiates influx db client
+def get_df_from_raw_enedis(path_raw_enedis: Path) -> pd.DataFrame:
+    """Read a csv generated for enedis and returns a DataFrame
+
+    Parameters
+    ----------
+    path_raw_enedis : Path
+        Path to the enedis csv
 
     Returns
     -------
-    InfluxDBClient
-        Influx db client initiated
+    pd.DataFrame
+        Pandas DataFrame with enedis sensor value as column
     """
-    return InfluxDBClient(
-        url=INFLUXDB_URL, token=INFLUXDB_TOKE, org=DICT_INFLUXDB["DOCKER_INFLUXDB_INIT_ORG"], debug=False
-    )
+    # Read csv
+    df = pd.read_csv(path_raw_enedis, sep=";", encoding="latin-1")
+
+    # Date conversion and index
+    df["date"] = df["Date de la mesure"] + " " + df["Heure de la mesure"]
+    df.index = pd.to_datetime(df["date"])
+
+    # Filter empty values
+    df = df[~df["Valeur"].isna()]
+
+    return df[["Valeur"]].rename(columns={"Valeur": "Value"})
